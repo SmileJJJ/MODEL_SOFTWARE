7eba224d5edbd471b998d9329a0081da218f988f
NANXI_GSGS <281206814@qq.com>
2019-05-10 16:49:34 +0800

    opencv提供的探测更改完毕

--- scene_objects_recongnition/Program_OpenCV/detection_openCV.py
+++ scene_objects_recongnition/Program_OpenCV/detection_openCV.py
@@ -28,16 +28,36 @@ import os
 
 class OpenCV__Detection():
 
-    def __init__(self, model_path, prototxt_path, CONFIDENCE):
+    def __init__(self, model_path, prototxt_path, CONFIDENCE=0.2):
         self.CONFIDENCE = CONFIDENCE
         self.model_path = model_path
         self.prototxt_path = prototxt_path
+        # 加载用caffe训练好的模型，prototxt：模型结构文件，model：模型文件
         self.net = cv2.dnn.readNetFromCaffe(self.prototxt_path, self.model_path)
 
-    def detection_run(self, image):
-        self.blob = cv2.dnn.blobFromImage(image, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), True)
+        self.CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+                        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+                        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+                        "sofa", "train", "tvmonitor"]
+        self.COLORS = np.random.uniform(0, 255, size=(len(self.CLASSES), 3))
+
+    def detection_run(self, frame):
+        (h, w) = frame.shape[:2]
+        self.blob = cv2.dnn.blobFromImage(frame, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), True)
         self.net.setInput(self.blob)
-    
+        detections = self.net.forward()
+        for i in np.arange(0, detections.shape[1]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > self.CONFIDENCE:
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startx, starty, endx, endy) = box.astype('int')
+
+                label = '{}:{:.2f}%'.format(self.CLASSES[idx], confidence*100)
+                cv2.rectangle(frame, (startx, starty), (endx, endy), self.COLORS[idx], 2)
+                y = starty - 15 if starty - 15 > 15 else starty + 15
+                cv2.putText(frame, label, (startx, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.COLORS[idx, 2])
+        return frame
 
 
 if __name__ == '__main__':
@@ -48,3 +68,12 @@ if __name__ == '__main__':
     prototxt_path = models_path + 'MobileNetSSD_deploy.prototxt.txt'
 
     object_test = OpenCV__Detection(model_path, prototxt_path)
+
+    vc = cv2.VideoCapture(0)
+    while True:
+        frame = vc.read()[1]
+        image = object_test.detection_run(frame)
+        cv2.imshow('frame', image)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
--- scene_objects_recongnition/scene_objects_recongnition.py
+++ scene_objects_recongnition/scene_objects_recongnition.py
@@ -19,12 +19,16 @@ __mtime__ = 'None'
                   ┃┫┫  ┃┫┫
                   ┗┻┛  ┗┻┛
 """
+import cv2
 import os
 import sys
+import numpy as np 
 
 if __name__ == '__main__':
 
-    models_path = 'F:\\MODEL_SOFTWARE\\resources\\models\\imageai\\'
+    imageai_models_path = 'F:\\MODEL_SOFTWARE\\resources\\models\\imageai\\'
+    opencv_model_path = 'F:\\MODEL_SOFTWARE\\resources\\models\\opencv\\MobileNetSSD_deploy.caffemodel'
+    opencv_prototxt_path = 'F:\\MODEL_SOFTWARE\\resources\\models\\opencv\\MobileNetSSD_deploy.prototxt.txt'
 
     image_path = 'F:\\MODEL_SOFTWARE\\resources\\image_test\\test1.png'
     new_image_path = 'F:\\MODEL_SOFTWARE\\resources\\image_test\\test1_new.png'
@@ -32,10 +36,25 @@ if __name__ == '__main__':
     video_path = 'F:\\MODEL_SOFTWARE\\resources\\video_test\\traffic.mp4'
     new_video_path = 'F:\\MODEL_SOFTWARE\\resources\\video_test\\traffic_new.png'
 
-    detect_method = 'ImageAI'
+
+
+    # detect_method = 'ImageAI'
+    detect_method = 'OpenCV'
+
     if detect_method == 'ImageAI':
         from Program_ImageAI.detection_imageAI import Image_AI_Objects_Detection
-        object_test = Image_AI_Objects_Detection(models_path, detect_target='video')
+        object_test = Image_AI_Objects_Detection(imageai_models_path, detect_target='video')
         # object_test.detect_run(resource_path=image_path, save_path=new_image_path)
         object_test.detect_run(resource_path=video_path, save_path=new_video_path)
 
+    if detect_method == 'OpenCV':
+        from Program_OpenCV.detection_openCV import OpenCV__Detection
+        object_test = OpenCV__Detection(opencv_model_path, opencv_prototxt_path)
+        vc = cv2.VideoCapture(video_path)
+        while True:
+            frame = vc.read()[1]
+            image = object_test.detection_run(frame)
+            cv2.imshow('frame', image)
+            if cv2.waitKey(1) == 27:
+                break
+        cv2.destroyAllWindows()
